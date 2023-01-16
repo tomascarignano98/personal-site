a23879be262253811bd62c2a0652d3ba1e299944
tomascarignano98 <tomascarignano98@gmail.com>
2023-01-16 13:06:48 -0300

    feat: update Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <Navbar />
       <div className="flex min-h-[calc(100vh-3.5rem)] flex-col justify-between">
-        <main className="mx-auto mt-8 w-full max-w-screen-lg px-4">
+        <main className="mx-auto mt-20 w-full max-w-screen-lg px-4 sm:mt-28">
           {children}
         </main>
         <Footer />
