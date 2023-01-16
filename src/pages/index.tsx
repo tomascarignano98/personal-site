bcf5542648947ac4fb9d2190b4764f21abde2da6
tomascarignano98 <tomascarignano98@gmail.com>
2023-01-16 13:11:01 -0300

    feat: update HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import Button from '@/components/Button';
+import AboutSection from '@/components/AboutSection';
+import HeroSection from '@/components/HeroSection';
+import ProjectsSection from '@/components/ProjectsSection';
 import SEO from '@/components/SEO';
 
 export default function HomePage() {
@@ -8,23 +10,9 @@ export default function HomePage() {
     <>
       <SEO title="Tomas Carignano" />
 
-      <p>Hi there!</p>
-      <h1 className="print:hidden">
-        I&apos;m Tomas Carignano. <br />A Web Developer.
-      </h1>
-      <Button>Get In Touch</Button>
-      <br />
-      <br />
-      <br />
-
-      <h2>About Me</h2>
-      <p>{`I'm a curious person who likes to solve business problems through the magic of code.`}</p>
-
-      <br />
-      <br />
-
-      <h2>Projects</h2>
-      <p>{`Here's a list of my best work`}</p>
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
     </>
   );
 }
