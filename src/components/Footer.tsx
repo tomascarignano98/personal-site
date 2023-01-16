d6afa5734f919e935e35c7cf5d4f2ec277a2bd46
tomascarignano98 <tomascarignano98@gmail.com>
2023-01-16 13:06:31 -0300

    feat: update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,42 @@
 export default function Footer(): JSX.Element {
   return (
-    <footer className="mt-14 flex h-16 w-full items-center justify-center border-t print:hidden">
+    <footer
+      id="contact"
+      className="mx-auto mt-32 mb-16 w-full max-w-screen-lg border-t px-2 pt-8 text-center sm:mb-20"
+    >
+      <h2>Let's Connect</h2>
       <p>
-        Desarrollado por{' '}
-        <span className="font-bold text-primaryHover">Qsoft</span>
+        <EmailLink /> at tomascarignano98@gmail.com <br />
+        or find me on <LinkedinLink /> or <GithubLink />
       </p>
     </footer>
+  );
+}
+
+function EmailLink() {
+  return <a href="mailto:tomascarignano98@gmail.com">Shoot me an email</a>;
+}
+
+function LinkedinLink() {
+  return (
+    <a
+      href="https://www.linkedin.com/in/tomas-carignano-aznarez-679b03210/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      LinkedIn
+    </a>
+  );
+}
+
+function GithubLink() {
+  return (
+    <a
+      href="https://github.com/tomascarignano98"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      GitHub
+    </a>
   );
 }
