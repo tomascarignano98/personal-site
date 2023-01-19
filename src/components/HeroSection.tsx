2565e0e4c97d818ff1d4aa813cc2a0efb2268bad
tomascarignano98 <tomascarignano98@gmail.com>
2023-01-19 16:28:36 -0300

    feat: improve styling

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,11 +5,8 @@ import Button from './Button';
 export default function HeroSection() {
   return (
     <section className="mx-auto max-w-[60ch] text-center">
-      <h1 className="mb-2 text-3xl leading-none sm:text-5xl">
-        <div className="text-xl leading-none sm:text-3xl sm:leading-none">
-          Hi There!
-        </div>{' '}
-        I'm Tomas Carignano
+      <h1 className="mb-2 sm:text-5xl">
+        <div className="text-xl sm:text-3xl">Hi There!</div> I'm Tomas Carignano
       </h1>
       <p className="mb-5">
         And I <em className="font-bold not-italic">love</em> solving business
