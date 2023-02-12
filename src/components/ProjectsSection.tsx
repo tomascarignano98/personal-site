bdec9e09ef50ae67574c13ed83617d9dbadec9e8
tomascarignano98 <tomascarignano98@gmail.com>
2023-02-12 11:34:43 -0300

    feat(ProjectsSection): link projects

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -8,19 +8,21 @@ export default function ProjectsSection() {
       <h2 className="mb-8 text-center lg:text-left">Projects</h2>
       <div className="mx-auto flex flex-col justify-between gap-y-6 gap-x-3 lg:flex-row">
         <ProjectCard
-          title="Doroteas"
-          description="A full-stack web app to help a group of schools manage their students financially."
-          // TODO: block student name and dni
-          src="doroteas.png"
-        />
-        <ProjectCard
           title="Qspeed"
-          description="A full-stack web app to see mountain bike race results in real time."
+          description="A multilayered product to time mountain bike races."
+          href="/projects/qspeed"
           src="qspeed.png"
         />
         <ProjectCard
+          title="Doroteas"
+          description="A full-stack web app to help a group of schools manage their students financially."
+          href="/projects/doroteas"
+          src="doroteas.png"
+        />
+        <ProjectCard
           title="Passport Bot"
-          description="A browser automation script to book an appointment at the spanish consulate."
+          description="A browser automation script to book an appointment at the Spanish consulate."
+          href="/projects/passport-bot"
           src="passport-bot.jpg"
         />
       </div>
@@ -31,15 +33,16 @@ export default function ProjectsSection() {
 interface ProjectProps {
   title: string;
   description: string;
+  href: string;
   src: string;
 }
-function ProjectCard({ title, description, src }: ProjectProps) {
+function ProjectCard({ title, description, href, src }: ProjectProps) {
   return (
     <article className="mx-auto flex w-80 flex-col justify-between overflow-hidden rounded-lg border border-gray-200 pb-5 text-center transition-transform duration-300 lg:hover:scale-105">
       <header className="w-full">
         <img
           src={src}
-          className="mb-3 h-44 w-full object-cover object-top"
+          className="mb-5 h-44 w-full object-cover object-top"
           alt={title + ' project'}
         />
         <h3 className="mb-2">{title}</h3>
@@ -48,7 +51,7 @@ function ProjectCard({ title, description, src }: ProjectProps) {
         <p className="sm:text-base">{description}</p>
       </main>
       <footer className="mt-auto">
-        <Link href="/#projects">
+        <Link href={href}>
           <Button>See Project</Button>
         </Link>
       </footer>
